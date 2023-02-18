d8cce3b9cc6d29d4bc43430e8c19c6c4edf5f388
Inso Web <70245299+InsoWebComeBack@users.noreply.github.com>
2023-02-18 20:56:29 +0100

    Update skCrypter.h

--- files/skCrypter.h
+++ files/skCrypter.h
@@ -130,12 +130,6 @@ namespace skc
 	};
 }
 
-#define skCrypt(str) skCrypt_key(str, __TIME__[4], __TIME__[7])
-#define skCrypt_key(str, key1, key2) []() { \
-			constexpr static auto crypted = skc::skCrypter \
-				<sizeof(str) / sizeof(str[0]), key1, key2, skc::clean_type<decltype(str[0])>>((skc::clean_type<decltype(str[0])>*)str); \
-					return crypted; }()
-
 /*________________________________________________________________________________
 
 MIT License
